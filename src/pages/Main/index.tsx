896a596d503a433d55d99cef757f16a164038561
okimyj <okimyj@gmail.com>
2024-04-19 16:17:48 +0900

    Refactor : directory 구조 변경

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,11 @@
+import Body from "../layout/body/Body";
+import Header from "../layout/header/Header";
+
+const MainPage = () => {
+  return (
+    <Body>
+      <Header />
+    </Body>
+  );
+};
+export default MainPage;
