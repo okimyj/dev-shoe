f4cd092d42cb97ce351271b089fdb4c72e682d4a
okimyj <okimyj@gmail.com>
2024-04-20 00:33:39 +0900

    Feat : 미가입 아이디 로그인시 회원가입 안내 팝업 적용

--- src/pages/Auth/Signin/hooks.ts
+++ src/pages/Auth/Signin/hooks.ts
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import useAuthAPI from "@/apis/auth";
 import { useNavigate } from "react-router-dom";
+import { useAlertPopupStore } from "@/common/stores/useAlertPopupStore";
 type Inputs = {
   email: string;
   password: string;
@@ -9,7 +10,7 @@ const useAuthSignin = () => {
   const { register, handleSubmit } = useForm<Inputs>();
   const { signin } = useAuthAPI();
   const navigate = useNavigate();
-
+  const { open: openAlert } = useAlertPopupStore();
   const handleSignin = (data: Inputs) => {
     signin(data.email, data.password)
       .then((res) => {
@@ -17,9 +18,16 @@ const useAuthSignin = () => {
       })
       .catch((res) => {
         if (res.error.code && res.error.code === "auth/invalid-email") {
-          navigate("/signup");
+          openAlert(
+            "email이 존재하지 않습니다.",
+            "회원가입 페이지로 이동합니다.",
+            (confirm) => {
+              if (confirm) navigate("/signup");
+            },
+            undefined,
+            "확인",
+          );
         }
-        console.log(res.error);
       });
   };
   return { register, handleSubmit, handleSignin };
--- src/pages/layout/body/inner/InnerBody.tsx
+++ src/pages/layout/body/inner/InnerBody.tsx
@@ -1,4 +1,8 @@
 const InnerBody = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-  return <div className={["w-body-inner-l", "flex flex-col", className].join(" ")}>{children}</div>;
+  return (
+    <div className={["w-body-inner-l", "flex flex-col items-center", className].join(" ")}>
+      {children}
+    </div>
+  );
 };
 export default InnerBody;
